49c7f5511d25ffba7ae72277a8654b03bf44307a
Luis Valdés <luis.valdes.cuesta@gmail.com>
2020-10-07 19:18:19 +0200

    Add base project

---
+++ SEATCode/Sources/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  SEATCode
+//
+//  Created by Luis Valdés on 07/10/2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ SEATCodeTests/Sources/SEATCodeTests.swift
@@ -0,0 +1,24 @@
+//
+//  SEATCodeTests.swift
+//  SEATCodeTests
+//
+//  Created by Luis Valdés on 07/10/2020.
+//
+
+import XCTest
+@testable import SEATCode
+
+final class SEATCodeTests: XCTestCase {
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
